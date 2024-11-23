691e32c7cc295d2af2b0c3111d932548b439b7cd
rahatalmas <rahat.almas5599@gmail.com>
2024-11-23 20:09:28 +0600

    examfilter api call

--- quizapp/lib/Widgets/examFilter.dart
+++ quizapp/lib/Widgets/examFilter.dart
@@ -1,16 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:quizapp/constant.dart';
+import 'package:provider/provider.dart';
 import 'package:quizapp/models/exammodel.dart';
+import 'package:quizapp/providers/examProvider.dart';
 
 class ExamFilterWidget extends StatefulWidget {
-  final List<Exam> examList;
   final Function(Exam) onExamSelected;
 
   const ExamFilterWidget({
     super.key,
-    required this.examList,
-    required this.onExamSelected,
+    required this.onExamSelected, required List<Exam> examList,
   });
 
   @override
@@ -20,22 +19,26 @@ class ExamFilterWidget extends StatefulWidget {
 class _ExamFilterWidgetState extends State<ExamFilterWidget> {
   Exam? _selectedExam;
 
-  void _showExamFilterModal(BuildContext context) {
+  void _showExamFilterModal(BuildContext context) async {
+    final examProvider = Provider.of<ExamProvider>(context, listen: false);
+
+    await examProvider.fetchExams('http://192.168.31.184:8080/api/exam/list');
+
+    final examList = examProvider.exams;
+
+    //the bottom sheet modal
     showModalBottomSheet(
       context: context,
       shape: const RoundedRectangleBorder(
         borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
       ),
-      isScrollControlled:
-          true, // This ensures that the bottom sheet takes up all available space
+      isScrollControlled: true, // Ensures that the bottom sheet takes up all available space
       builder: (context) {
         return Padding(
           padding: const EdgeInsets.all(16.0),
           child: Container(
             height: MediaQuery.of(context).size.height - 200,
-            decoration: BoxDecoration(
-                //later ...
-                ),
+            decoration: BoxDecoration(),
             child: Column(
               mainAxisSize: MainAxisSize.min,
               children: [
@@ -47,10 +50,10 @@ class _ExamFilterWidgetState extends State<ExamFilterWidget> {
                 Expanded(
                   child: ListView.separated(
                     shrinkWrap: true,
-                    itemCount: widget.examList.length,
+                    itemCount: examList.length,
                     separatorBuilder: (context, index) => const Divider(),
                     itemBuilder: (context, index) {
-                      final exam = widget.examList[index];
+                      final exam = examList[index];
                       return ListTile(
                         title: Text(exam.name),
                         subtitle: Text(
@@ -61,7 +64,7 @@ class _ExamFilterWidgetState extends State<ExamFilterWidget> {
                             _selectedExam = exam;
                           });
                           widget.onExamSelected(exam);
-                          Navigator.pop(context);
+                          Navigator.pop(context); // Close the modal
                         },
                       );
                     },
@@ -77,63 +80,35 @@ class _ExamFilterWidgetState extends State<ExamFilterWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return (
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            GestureDetector(
-                onTap: () => _showExamFilterModal(context),
-                child: Container(
-                  width: double.maxFinite,
-                  padding: EdgeInsets.symmetric(vertical: 15,horizontal: 10),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    border: Border.all(color: Colors.black12),
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Text(
-                        _selectedExam == null
-                            ? "Select Exam"
-                            : _selectedExam!.name,
-                        style: const TextStyle(
-                            fontSize: 16, fontWeight: FontWeight.bold),
-                      ),
-                      SvgPicture.asset("assets/images/filter.svg")
-                    ],
-                  ),
-                )
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        GestureDetector(
+          onTap: () => _showExamFilterModal(context),
+          child: Container(
+            width: double.maxFinite,
+            padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 10),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              border: Border.all(color: Colors.black12),
             ),
-           /* SizedBox(height: 10,),
-            if (_selectedExam != null) ...[
-              Container(
-                width: double.maxFinite,
-                padding: EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                  color: Colors.indigo[50],
-                  borderRadius: BorderRadius.circular(10)
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  _selectedExam == null
+                      ? "Select Exam"
+                      : _selectedExam!.name,
+                  style: const TextStyle(
+                      fontSize: 16, fontWeight: FontWeight.bold),
                 ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text("Details for: ${_selectedExam!.name}"),
-                    Text("Date & Time: ${_selectedExam!.dateTime}"),
-                    Text("Location: ${_selectedExam!.location}"),
-                    Text("Duration: ${_selectedExam!.duration} minutes"),
-                    Text("Total Questions: ${_selectedExam!.totalQuestions}"),
-                    Text(
-                        "Options per Question: ${_selectedExam!.optionsPerQuestion}"
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          */
-          ],
-
-        ));
+                SvgPicture.asset("assets/images/filter.svg"),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
