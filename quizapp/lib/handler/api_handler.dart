e231948aade5b401a71e47a2a89b34c81331e334
Miskat_UL <miskat15-10737@diu.edu.bd>
2024-11-28 00:10:51 +0600

    Add class BaseAuthHandlers

--- quizapp/lib/handler/api_handler.dart
+++ quizapp/lib/handler/api_handler.dart
@@ -1,37 +1,18 @@
-import 'dart:convert';
-
-import 'package:flutter/cupertino.dart';
-import 'package:http/http.dart' as http;
 import 'package:quizapp/handler/models/login.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
-class ApiHandler {
+/// Abstract class for authentication.
+abstract class BaseAuthHandler {
+
   // function to return login information.
-  Future<String?> loginApi() async {
-    String apiLink = "http://192.168.31.184:8080/api/user/login/";
-    try {
-      var url = Uri.parse(apiLink);
-      var response = await http.post(
-        headers: {
-          "Content-Type": "application/json",
-        },
-        url,
-        body: jsonEncode({"username": "konan", "password": "akatsuki"}),
-      );
-      print(response.body);
-      var res = Login.fromJson(jsonDecode(response.body));
+  Future<Login?> login(String user, String password);
 
-      SharedPreferences prefs = await SharedPreferences.getInstance();
-      await prefs.setString('accessToken', res.accesstoken);
+  bool isLoggedIn();
 
-      debugPrint(res.accesstoken.toString());
-      debugPrint(res.message);
-      debugPrint(res.permission.toString());
-      debugPrint(res.username);
-      return res.accesstoken;
-    } catch (e) {
-      debugPrint(e.toString());
-    }
-    return null;
-  }
+  String? getUserToken();
+
+  Future<void> refreshUserToken();
+
+  Future<void> setAccessToken();
+
+  Future<void> getAccessToken();
 }
