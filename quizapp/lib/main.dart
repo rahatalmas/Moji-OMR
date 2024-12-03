5a04e17b2c0dedba6a0ed375c154d8f42bc181d0
Miskat_UL <miskat15-10737@diu.edu.bd>
2024-12-03 23:48:52 +0600

    Logout

--- quizapp/lib/main.dart
+++ quizapp/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
 import 'package:provider/provider.dart';
 import 'package:quizapp/Screens/adminPage.dart';
 import 'package:quizapp/Screens/home.dart';
+import 'package:quizapp/handler/apis/login.dart';
 import 'package:quizapp/providers/actionProvider.dart';
 import 'package:quizapp/providers/examProvider.dart';
 import 'package:quizapp/providers/questionProvider.dart';
@@ -80,10 +81,12 @@ class _Root extends State<Root> with SingleTickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
-    bool editModeOn = Provider.of<ActionStatusProvider>(context, listen: true).actionInfo;
+    bool editModeOn =
+        Provider.of<ActionStatusProvider>(context, listen: true).actionInfo;
 
     return Scaffold(
-      key: _scaffoldKey, // Assign the scaffold key
+      key: _scaffoldKey,
+      // Assign the scaffold key
       backgroundColor: neutralWhite,
       appBar: AppBar(
         centerTitle: true,
@@ -98,35 +101,35 @@ class _Root extends State<Root> with SingleTickerProviderStateMixin {
         ),
         title: Text(
           "Home",
-          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25, color: appTextPrimary),
+          style: TextStyle(
+              fontWeight: FontWeight.bold, fontSize: 25, color: appTextPrimary),
         ),
         actions: [
           Icon(Icons.content_paste_search, color: appTextPrimary),
           const SizedBox(width: 10),
           editModeOn
               ? InkWell(
-            child: Icon(Icons.sunny, color: appTextPrimary),
-            onTap: () {
-              Provider.of<ActionStatusProvider>(context, listen: false).turnActionStatusOff();
-            },
-          )
+                  child: Icon(Icons.sunny, color: appTextPrimary),
+                  onTap: () {
+                    Provider.of<ActionStatusProvider>(context, listen: false)
+                        .turnActionStatusOff();
+                  },
+                )
               : Icon(Icons.mode_night, color: appTextPrimary),
           const SizedBox(width: 15),
         ],
       ),
       body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16.0,vertical: 12.0),
+        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
         child: TabBarView(
           controller: _tabController,
           children: _children,
         ),
       ),
       bottomNavigationBar: Container(
-        margin: const EdgeInsets.symmetric(vertical: 10,horizontal: 16),
+        margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 16),
         decoration: BoxDecoration(
-          color: neutralBG,
-              borderRadius: BorderRadius.circular(12)
-        ),
+            color: neutralBG, borderRadius: BorderRadius.circular(12)),
         child: TabBar(
           controller: _tabController,
           dividerColor: Colors.transparent,
@@ -134,11 +137,14 @@ class _Root extends State<Root> with SingleTickerProviderStateMixin {
             color: brandMinus2, // Green color for the selected tab
             borderRadius: BorderRadius.circular(12), // Optional rounded corners
           ),
-          indicatorSize: TabBarIndicatorSize.tab, // Ensures the indicator spans the full tab width
-          labelPadding: EdgeInsets.zero, // Removes padding around the label
-          labelColor:brandPlus2,
+          indicatorSize: TabBarIndicatorSize.tab,
+          // Ensures the indicator spans the full tab width
+          labelPadding: EdgeInsets.zero,
+          // Removes padding around the label
+          labelColor: brandPlus2,
           // Text and icon color for the selected tab
-          unselectedLabelColor: Colors.grey, // Text and icon color for unselected tabs
+          unselectedLabelColor: Colors.grey,
+          // Text and icon color for unselected tabs
           tabs: const [
             Tab(icon: Icon(Icons.home), text: "Home"),
             Tab(icon: Icon(Icons.sailing), text: "Result"),
@@ -171,10 +177,9 @@ class _Root extends State<Root> with SingleTickerProviderStateMixin {
               leading: Icon(Icons.person),
               title: Text("Admin"),
               onTap: () {
-                 Navigator.push(
+                Navigator.push(
                   context,
-                  MaterialPageRoute(
-                      builder: (context) => AdminPage()),
+                  MaterialPageRoute(builder: (context) => AdminPage()),
                 );
                 _tabController.animateTo(0); // Navigate to the Admin Page
               },
@@ -203,8 +208,21 @@ class _Root extends State<Root> with SingleTickerProviderStateMixin {
             ListTile(
               leading: Icon(Icons.exit_to_app),
               title: Text("Logout"),
-              onTap: () {
-                Navigator.pop(context);
+              onTap: () async {
+                Auth().logout().then((value) {
+                  if (!context.mounted) return;
+                  if (value) {
+                    Navigator.pushNamedAndRemoveUntil(context,
+                        RouteNames.landing, (Route<dynamic> route) => false);
+                  } else {
+                    if (!context.mounted) return;
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(
+                        content: Text('Logout failed. Please try again.'),
+                      ),
+                    );
+                  }
+                });
               },
             ),
           ],
