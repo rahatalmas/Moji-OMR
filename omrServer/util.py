7f9baf022e709c8fea9b3bf4b26c1fefd62db7b8
rahatalmas <rahat.almas5599@gmail.com>
2024-12-15 07:36:27 +0600

    omr

--- omrServer/util.py
+++ omrServer/util.py
@@ -3,15 +3,16 @@ def recCountour(coutours):
     rectCon = []
     for i in coutours:
         area = cv.contourArea(i)
-        if area > 50:
-            peri = cv.arcLength(i,True)
-            approx = cv.approxPolyDP(i,0.02*peri,True)
-            if len(approx) == 16:
-                rectCon.append(i)
+        print(area)
+        #if area > 100:
+        peri = cv.arcLength(i,True)
+        approx = cv.approxPolyDP(i,0.1*peri,True)
+        print(len(approx))
+        rectCon.append(i)
     rectCon = sorted(rectCon,key=cv.contourArea,reverse=True)
     return rectCon
 
 def getCornerPoints(cont):
         peri = cv.arcLength(cont,True)
-        approx = cv.approxPolyDP(cont,0.02*peri,True)
+        approx = cv.approxPolyDP(cont,0.1*peri,True)
         return approx
